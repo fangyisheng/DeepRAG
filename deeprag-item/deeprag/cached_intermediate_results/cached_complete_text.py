729257d2a8234bf2abdd06389cc24ebcb76f4eac
fys <fys380799654@gmail.com>
2025-02-14 10:27:43 +0800

    提交新的测试代码

--- deeprag-item/deeprag/cached_intermediate_results/cached_complete_text.py
+++ deeprag-item/deeprag/cached_intermediate_results/cached_complete_text.py
@@ -1,10 +1,28 @@
 import pandas as pd
 from tabulate import tabulate
 import uuid
+from pathlib import Path
+
+
 # 创建示例 DataFrame
-async def cache_complete_text(complete_text):
+async def cache_complete_text(complete_text,title):
     data = {
         "id":f"doc_{str(uuid.uuid4())}",
         "text": complete_text,
-        
+        "title": title
     }
+    df = pd.DataFrame(data)
+    
+    df.to_csv()
+    
+    # parquet_path = 
+    df.to_parquet()
+
+    return df
+    #返回一个DataFrame对象
+
+
+# from pathlib import Path
+
+# def test():
+#     print(Path.cwd())
---
+++ deeprag-item/test/test_workdir.py
@@ -0,0 +1,3 @@
+from deeprag.cached_intermediate_results.cached_complete_text import test
+
+test()
