99c72bbbd39c6274fe955966afb34046f1607a1b
fys <fys380799654@gmail.com>
2025-03-06 10:31:25 +0800

    提交新代码

--- deeprag-item/deeprag/db/service/user/user_service.py
+++ deeprag-item/deeprag/db/service/user/user_service.py
@@ -1,7 +1,13 @@
-# from deeprag.db.dao.user.user_dao import UserDAO
+from deeprag.db.dao.user.user_dao import UserDAO
 
-# class UserService:
-#     def __init__(self):
-#         self.dao = UserDAO()
+class UserService:
+    def __init__(self):
+        self.dao = UserDAO()
     
-#     async def 
+    async def get_user_name_by_id(self,id: str):
+        user = await self.dao.get_user_name_by_id(id)
+        return user.__dict__
+    
+    async def delete_user(self, id:str):
+        user = await self.dao.delete_user_by_id(id)
+        return user.__dict__
